4f96600d46c31de6c40bd5fe63ff50cb56bbb755
celdond <celdond@gmail.com>
2024-07-23 00:17:47 -0700

    adjustment final

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,5 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":5167"))
 }
